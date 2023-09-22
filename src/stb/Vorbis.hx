ebe2ba16acbc751fcac739101e1d8b25e2cd0493
swordcube <swordsoftarchive@gmail.com>
2023-09-22 16:35:27 -0500

    Replace all cpp.Pointer.addressOf(arg) when calling the function with arg, it'll handle it on it's own now

--- src/stb/Vorbis.hx
+++ src/stb/Vorbis.hx
@@ -9,8 +9,17 @@ package stb;
 @:buildXml('<include name="${haxelib:hxstb_vorbis}/include.xml" />')
 @:include('vendor/include/stb_vorbis.h')
 @:unreflective
-extern class Vorbis
-{
+extern class Vorbis {
 	@:native('stb_vorbis_decode_filename')
-	static function decodeFileName(fileName:cpp.ConstCharStar, channels:cpp.Star<Int>, sampleRate:cpp.Star<Int>, output:cpp.RawPointer<cpp.RawPointer<cpp.Int16>>):Int;
+	@:noCompletion
+	private static function __decodeFileName(fileName:cpp.ConstCharStar, channels:cpp.Star<Int>, sampleRate:cpp.Star<Int>, output:cpp.RawPointer<cpp.RawPointer<cpp.Int16>>):Int;
+
+	public static inline function decodeFileName(fileName:cpp.ConstCharStar, channels:cpp.Star<Int>, sampleRate:cpp.Star<Int>, output:cpp.RawPointer<cpp.Int16>) {
+		var totalFrameCount:Int = 0;
+		untyped __cpp__('short *output;
+		{0} = {1}({2}, {3}, {4}, &output);
+		{5} = output', totalFrameCount,
+			__decodeFileName, filePath, cpp.RawPointer.addressOf(channels), cpp.RawPointer.addressOf(sampleRate), sampleData);
+		return totalFrameCount;
+	}
 }
